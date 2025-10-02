9fcd2652d4867dde6aae4c700c576bf8f554f648
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-10-02 17:56:35 -0400

    cambios

--- src/log/log.module.ts
+++ src/log/log.module.ts
@@ -2,33 +2,37 @@ import { forwardRef, Module } from '@nestjs/common';
 import { LogService } from './log.service';
 import { LogController } from './log.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Log, LogDescarga, logDescargaSchema, LogIngresoUser, LogIngresoUserSchema, logSchema } from './schemas/log.schema';
+import {
+  Log,
+  LogDescarga,
+  logDescargaSchema,
+  LogIngresoUser,
+  LogIngresoUserSchema,
+  logSchema,
+} from './schemas/log.schema';
 import { HttpModule } from '@nestjs/axios';
 
-
 @Module({
-  imports:[
+  imports: [
     HttpModule,
-  
-    MongooseModule.forFeature(
-      [
-        {
-          name: Log.name,
-          schema: logSchema,
-        },
-        {
-          name: LogDescarga.name,
-          schema: logDescargaSchema,
-        },
-          {
-          name: LogIngresoUser.name,
-          schema: LogIngresoUserSchema,
-        },
-      ]
-    ),
+
+    MongooseModule.forFeature([
+      {
+        name: Log.name,
+        schema: logSchema,
+      },
+      {
+        name: LogDescarga.name,
+        schema: logDescargaSchema,
+      },
+      {
+        name: LogIngresoUser.name,
+        schema: LogIngresoUserSchema,
+      },
+    ]),
   ],
   controllers: [LogController],
   providers: [LogService],
-  exports:[LogService]
+  exports: [LogService],
 })
 export class LogModule {}
